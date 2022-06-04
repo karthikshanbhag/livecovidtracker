64a41bd87033de80af51517bb1e9ddb0aed7b23a
karthikshanbhag <karthikshanbhag969@gmail.com>
2022-06-05 01:10:21 +0530

    best code for now

--- src/component/covid.js
+++ src/component/covid.js
@@ -1,12 +1,14 @@
-import React, { useEffect } from 'react'
+import React, { useEffect,useState } from 'react'
+import './covid.css'
 
 const Covid = () => {
-
+    const [data, setdata] = useState([])
     const  getCovidData = async () => {
         try {
             const res = await fetch('https://data.covid19india.org/data.json')
             const actualData = await res.json(); 
             console.log(actualData.statewise[0])
+            setdata(actualData.statewise[0])
         } catch (err) {
             console.log(err);
             
@@ -14,17 +16,69 @@ const Covid = () => {
     }
 
     useEffect(() => {
-        // getCovidData();
+        getCovidData();
     }, [])
   return (
-    <>
-        <h1>LIVE</h1>
+    <>  
+        <section>
+        <h1>&#128308;LIVE</h1>
         <h2>COVID-19 CORONAVIRUS TRACKER</h2>
         <ul>
+            <div className="row">
             <li className="card">
-
+                <div className="card_main">
+                    <div className="class_inner">
+                        <p className="card_name"><span> OUR </span> COUNTRY </p>
+                        <p className="card_total card__small">INDIA</p>
+                    </div>
+                </div>
             </li>
+            <li className="card">
+                <div className="card_main">
+                    <div className="class_inner">
+                        <p className="card_name"><span> TOTAL </span> RECOVERED </p>
+                        <p className="card_total card__small">{data.recovered}</p>
+                    </div>
+                </div>
+            </li>
+            <li className="card">
+                <div className="card_main">
+                    <div className="class_inner">
+                        <p className="card_name"><span> TOTAL </span> CONFIRMED </p>
+                        <p className="card_total card__small">{data.confirmed}</p>
+                    </div>
+                </div>
+            </li>
+            </div>
+           
+            <div className="row">
+            <li className="card">
+                <div className="card_main">
+                    <div className="class_inner">
+                        <p className="card_name"><span> TOTAL </span> DEATH </p>
+                        <p className="card_total card__small">{data.deaths}</p>
+                    </div>
+                </div>
+            </li>
+            <li className="card">
+                <div className="card_main">
+                    <div className="class_inner">
+                        <p className="card_name"><span> TOTAL </span> ACTIVE </p>
+                        <p className="card_total card__small">{data.active}</p>
+                    </div>
+                </div>
+            </li>
+            <li className="card">
+                <div className="card_main">
+                    <div className="class_inner">
+                        <p className="card_name"><span> LAST </span> UPDATED </p>
+                        <p className="card_total card__small">{data.lastupdatedtime}</p>
+                    </div>
+                </div>
+            </li>
+            </div>
         </ul>
+        </section>
     </>
   )
 }
